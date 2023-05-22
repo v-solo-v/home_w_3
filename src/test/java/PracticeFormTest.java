eb984aa71868285f83f672bd99553c04b70d9393
Solo VV <makler63@yandex.ru>
2023-05-22 22:33:55 +0300

    Add Corrections on comments

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,21 +1,27 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTest {
-    @Test
-    void automationPracticeForm () {
+
+    @BeforeAll
+    public static void beforeAllMethod() {
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.browser = System.getProperty("browser", "firefox");
+        Configuration.pageLoadStrategy = "eager";
         open("https://demoqa.com/automation-practice-form");
         Selenide.executeJavaScript("$('#fixedban').remove()");
         Selenide.executeJavaScript("$('footer').remove()");
-
+    }
+    @Test
+    void automationPracticeForm () {
         $("#firstName").setValue("Diego");
         $("#lastName").setValue("Maradona");
         $("#userEmail").setValue("special_one@gmail.ru");
@@ -24,7 +30,7 @@ public class PracticeFormTest {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
         $(".react-datepicker__year-dropdown-container").$(byText("1960")).click();
-        $(".react-datepicker__month-container").$(byText("30")).click();
+        $(".react-datepicker__day--0" + "30" + ":not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Commerce").pressEnter();
         $("#hobbies-checkbox-1").parent().$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("myfoto.jpg");
@@ -34,6 +40,7 @@ public class PracticeFormTest {
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Panipat")).click();
         $("#submit").click();
+        $(".modal-content").should(appear);
         $(".modal-open").shouldHave(text("Thanks for submitting the form"));
         $(".modal-body").shouldHave(text("Diego Maradona"));
         $(".modal-body").shouldHave(text("special_one@gmail.ru"));
